e773a49ab2741d17af246096055d10d4985c83f0
Chatenium <personal@alms.hu>
2025-12-23 18:37:30 +0100

    Implement DM message fetching

---
+++ backend/chat/dm/dm_handler.py
@@ -0,0 +1,66 @@
+from typing import List
+
+from backend.http import Http, HttpMethod, ResultType
+from backend.session_manager import SessionManager
+from dataclasses import dataclass
+from backend.types import TimeStamp
+
+@dataclass()
+class Message:
+    msgid: str
+    author: str
+    message: str
+    sent_at: TimeStamp
+    isEdited: bool
+    chatid: str
+    files: List[Attachment]
+    seen: bool
+    replyTo: str
+    replyToId: str
+    forwardedFrom: str
+    forwardedFromName: str
+    metaData: None = None
+
+@dataclass()
+class Attachment:
+    fileID: str
+    fileName: str
+    format: str
+    type: str
+    path: str
+    height: int
+    width: int
+    hasThumbnail: bool
+
+class DmHandler(object):
+    _instance = None
+
+    # _listeners: List[Callable[[List[Chat]], None]] = []
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            print('Creating new instance')
+            cls._instance = cls.__new__(cls)
+        return cls._instance
+
+    async def get_messages(self, chatid) -> List[Message]:
+        print("Getting messages")
+
+        if SessionManager.instance().currentSession is None:
+            raise ValueError("No session")
+
+        result = await Http(
+            HttpMethod.GET,
+            f"chat/dm/messages?userid={SessionManager.instance().currentSession[1].userid}&chatid={chatid}&from={0}",
+            None,
+            Message,
+        )
+
+        if result.type == ResultType.SUCCESS:
+            return result.success
+        else:
+            raise ValueError(result.error)
