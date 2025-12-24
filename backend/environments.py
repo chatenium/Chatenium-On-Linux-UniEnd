f378950cb8b79dd0c9cfae3a0aca0561bfca337e
Noel Maróti <muveszies-kereghanto.9t@icloud.com>
2025-12-24 19:04:41 +0100

    Update environments.py

--- backend/environments.py
+++ backend/environments.py
@@ -1,6 +1,19 @@
 import os
 
 class Environments(object):
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            print('Creating new Env instance')
+            cls._instance = cls.__new__(cls)
+            # Put any initialization here.
+        return cls._instance
+    
     api_url = os.environ.get("API_URL", "https://api.chatenium.hu")
     ws_url = os.environ.get("WS_URL", "wss://api.chatenium.hu")
 
